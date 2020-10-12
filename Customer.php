b38b2c4cd6eaa52e4afc92edfbc133baec2b869e
Sebastian Pfanzelt <pfa@kag-erding.de>
2020-10-12 16:29:47 +0200

    Atttributes added

--- Customer.php
+++ Customer.php
@@ -3,5 +3,13 @@
 
 class Customer
 {
+    private $name;
+    private $age;
+
+    public function __construct($name, $age)
+    {
+        $this->name = $name;
+        $this->age  = $age;
+    }
 
 }
